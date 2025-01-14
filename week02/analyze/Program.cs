97de643cfa0ec79a3b118a72a6da4da17a3b4512
hwcall1102 <haydenc0293@gmail.com>
2025-01-13 23:23:56 -0700

    learning

--- week02/analyze/Program.cs
+++ week02/analyze/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.WriteLine("\n======================\nMysteryStack1\n======================");
-Console.WriteLine(MysteryStack1.Run("racecar"));
-Console.WriteLine(MysteryStack1.Run("stressed"));
-Console.WriteLine(MysteryStack1.Run("a nut for a jar of tuna"));
+﻿// Console.WriteLine("\n======================\nMysteryStack1\n======================");
+// Console.WriteLine(MysteryStack1.Run("racecar"));
+// Console.WriteLine(MysteryStack1.Run("stressed"));
+// Console.WriteLine(MysteryStack1.Run("a nut for a jar of tuna"));
 
 Console.WriteLine("\n======================\nMysteryStack2\n======================");
 Console.WriteLine(MysteryStack2.Run("5 3 7 + *"));
